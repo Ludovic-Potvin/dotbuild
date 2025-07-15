d295772bfea595e2d1a3c6797a5b96180784f4c7
lpotvin <ludopotvin@gmail.com>
2025-07-14 22:53:38 -0400

    basic hyprland configs

---
+++ dotbuild/scripts/arch/install_basics.sh
@@ -0,0 +1,8 @@
+# Make git log work
+sudo pacman -S less
+
+# Make the hyprland theme work
+sudo pacman -S wofi
+
+# The rest
+sudo pacman -S neovim
--- dotbuild/scripts/arch/install_nvim.sh
+++
@@ -1,8 +0,0 @@
-echo "Nvim setup"
-
-echo "Install nvim..."
-sudo pacman -S neovim
-
-echo "Add nvim configs..."
-
-
