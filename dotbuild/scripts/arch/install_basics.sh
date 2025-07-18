e2527b1a01a92459ecdadf47c0e0efe57b19c638
lpotvin <ludopotvin@gmail.com>
2025-07-18 13:27:17 -0400

    remove lame deps

--- dotbuild/scripts/arch/install_basics.sh
+++ dotbuild/scripts/arch/install_basics.sh
@@ -3,6 +3,13 @@ sudo pacman -S less
 
 # Make the hyprland theme work
 sudo pacman -S wofi
+sudo pacman -S waybar
+sudo pacman -S ttf-font-awesome
 
-# The rest
+# Bluetooth
+sudo pacman -S blueman
+sudo pacman -S bluez
+
+# Others
 sudo pacman -S neovim
+sudo pacman -S firefox
