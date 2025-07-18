99843861b85915a7f8b0212691112c3262affe25
lpotvin <ludopotvin@gmail.com>
2025-07-18 14:07:50 -0400

    hyprpaper and cleanup

--- dotbuild/scripts/arch/install_basics.sh
+++ dotbuild/scripts/arch/install_basics.sh
@@ -5,11 +5,14 @@ sudo pacman -S less
 sudo pacman -S wofi
 sudo pacman -S waybar
 sudo pacman -S ttf-font-awesome
+sudo pacman -S hyprpaper
 
 # Bluetooth
 sudo pacman -S blueman
 sudo pacman -S bluez
 
-# Others
+# Apps
 sudo pacman -S neovim
 sudo pacman -S firefox
+sudo pacman -S tmux
+sudo pacman -S obsidian
