1d28f5643af8a495e2fcb08397e83ca1159d543d
lpotvin <ludopotvin@gmail.com>
2025-08-27 14:50:02 -0400

    adjusted lua project for nvim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,33 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({})
-    end
+    {
+        "mason-org/mason.nvim",
+        opts = {},
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+        end
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                },
+                automatic_installation = true,
+            })
+        end
+    },
 }
 
