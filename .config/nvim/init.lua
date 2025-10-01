64a2f24215a8e16ba8d7b9e510a4a0c610bc9b20
lpotvin <ludopotvin@gmail.com>
2025-09-30 21:17:02 -0400

    move keymaps to keymaps and remove tmux escape delay

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- Map Ctrl+C to Escape in insert, normal, and visual modes
-vim.keymap.set("i", "<C-c>", "<Esc>", { noremap = true })
-vim.keymap.set("n", "<C-c>", "<Esc>", { noremap = true })
-vim.keymap.set("v", "<C-c>", "<Esc>", { noremap = true })
-
 -- Keep cursor center
 vim.opt.scrolloff = 20
 vim.opt.colorcolumn = "80"
