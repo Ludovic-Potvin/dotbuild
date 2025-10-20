4a686e3b5207a7165e0dea91bd81d7854036fd28
lpotvin <ludopotvin@gmail.com>
2025-10-20 09:40:22 -0400

    fix cursor on nvim exit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,3 +4,9 @@ require("config.lazy")
 -- Keep cursor center
 vim.opt.scrolloff = 20
 vim.opt.colorcolumn = "80"
+
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = function()
+    vim.opt.guicursor = "a:ver25"
+  end,
+})
